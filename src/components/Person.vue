f85887738060e7f233a39ce33442513737be1be6
Chris Scott <cyberstrike@gmail.com>
2017-10-29 02:10:55 -0400

    Squashed commit of the following:
    commit 1030ad1908da181b687fc099d24677faf15e5c89
    Author: Chris Scott <cyberstrike@gmail.com>
    Date:   Sun Oct 29 02:10:09 2017 -0400
    
        attempt to fix animation and spacing

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -47,102 +47,104 @@ export default {
 
 <template>
   <v-slide-x-transition>
-    <v-container fluid v-show="show">
-      <v-layout row>
-        <v-flex xs12 text-xs-center>
-          <v-avatar class="mb-3" size=100>
-            <img :src="person.picture.large" :alt="fullName">
-          </v-avatar>
-        </v-flex>
-      </v-layout>
-      <v-layout row>
-        <v-flex xs12>
-          <h2 class="text-xs-center">{{fullName}}</h2>
-        </v-flex>
-      </v-layout>
-      <v-layout align-center justify-center>
-        <v-flex xs12 sm8 md6 lg5>
-          <v-card>
-            <v-list two-line>
+    <v-container app fluid v-show="show">
+      <v-layout column>
+        <v-layout row>
+          <v-flex xs12 text-xs-center>
+            <v-avatar class="mb-3" size=100>
+              <img :src="person.picture.large" :alt="fullName">
+            </v-avatar>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <h2 class="text-xs-center">{{fullName}}</h2>
+          </v-flex>
+        </v-layout>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md6 lg5>
+            <v-card>
+              <v-list two-line>
 
-              <!-- Nationality -->
-              <v-list-tile>
-                <v-list-tile-action>
-                  <v-icon class="indigo--text">language</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                  <v-list-tile-title>{{nationality}}</v-list-tile-title>
-                  <v-list-tile-sub-title>Nationality</v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list-tile>
+                <!-- Nationality -->
+                <v-list-tile>
+                  <v-list-tile-action>
+                    <v-icon class="indigo--text">language</v-icon>
+                  </v-list-tile-action>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{nationality}}</v-list-tile-title>
+                    <v-list-tile-sub-title>Nationality</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
 
-              <!-- Gender -->
-              <v-list-tile>
-                <v-list-tile-action>
-                  <v-icon class="indigo--text">accessibility</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                  <v-list-tile-title>{{person.gender | toTitleCase}}</v-list-tile-title>
-                  <v-list-tile-sub-title>Gender</v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list-tile>
+                <!-- Gender -->
+                <v-list-tile>
+                  <v-list-tile-action>
+                    <v-icon class="indigo--text">accessibility</v-icon>
+                  </v-list-tile-action>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{person.gender | toTitleCase}}</v-list-tile-title>
+                    <v-list-tile-sub-title>Gender</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
 
-              <v-subheader inset>Contact</v-subheader>
+                <v-subheader inset>Contact</v-subheader>
 
-              <!-- Smartphone -->
-              <v-list-tile>
-                <v-list-tile-action>
-                  <v-icon class="indigo--text">smartphone</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                  <v-list-tile-title>{{person.cell}}</v-list-tile-title>
-                  <v-list-tile-sub-title>Mobile</v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list-tile>
+                <!-- Smartphone -->
+                <v-list-tile>
+                  <v-list-tile-action>
+                    <v-icon class="indigo--text">smartphone</v-icon>
+                  </v-list-tile-action>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{person.cell}}</v-list-tile-title>
+                    <v-list-tile-sub-title>Mobile</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
 
-              <!-- Home -->
-              <v-list-tile>
-                <v-list-tile-action>
-                  <v-icon class="indigo--text">home</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                  <v-list-tile-title>{{person.phone}}</v-list-tile-title>
-                  <v-list-tile-sub-title>Home</v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list-tile>
+                <!-- Home -->
+                <v-list-tile>
+                  <v-list-tile-action>
+                    <v-icon class="indigo--text">home</v-icon>
+                  </v-list-tile-action>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{person.phone}}</v-list-tile-title>
+                    <v-list-tile-sub-title>Home</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
 
-              <!-- E-Mail -->
-              <v-list-tile>
-                <v-list-tile-action>
-                  <v-icon class="indigo--text">mail</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                  <v-list-tile-title>{{person.email}}</v-list-tile-title>
-                  <v-list-tile-sub-title>Personal</v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list-tile>
+                <!-- E-Mail -->
+                <v-list-tile>
+                  <v-list-tile-action>
+                    <v-icon class="indigo--text">mail</v-icon>
+                  </v-list-tile-action>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{person.email}}</v-list-tile-title>
+                    <v-list-tile-sub-title>Personal</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
 
 
-              <v-subheader inset>Addresses</v-subheader>
+                <v-subheader inset>Addresses</v-subheader>
 
-              <!-- Address -->
-              <v-list-tile>
-                <v-list-tile-action>
-                  <v-icon class="indigo--text">location_on</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                  <v-list-tile-title>{{person.location.street | toTitleCase}},</v-list-tile-title>
-                  <v-list-tile-sub-title>
-                    {{person.location.city | toTitleCase}},
-                    {{person.location.state | toTitleCase}}
-                    {{person.location.postcode}}
-                  </v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list-tile>
+                <!-- Address -->
+                <v-list-tile>
+                  <v-list-tile-action>
+                    <v-icon class="indigo--text">location_on</v-icon>
+                  </v-list-tile-action>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{person.location.street | toTitleCase}},</v-list-tile-title>
+                    <v-list-tile-sub-title>
+                      {{person.location.city | toTitleCase}},
+                      {{person.location.state | toTitleCase}}
+                      {{person.location.postcode}}
+                    </v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
 
-            </v-list>
-          </v-card>
-        </v-flex>
+              </v-list>
+            </v-card>
+          </v-flex>
+        </v-layout>
       </v-layout>
     </v-container>
   </v-slide-x-transition>
